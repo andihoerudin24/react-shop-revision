c49a6dc58a5bf8104a0b15200f82b72db01b965f
Andi Hoerduin <andihoerudin24@gmail.com>
2020-07-19 17:27:32 +0700

    175 done

--- models/order.js
+++ models/order.js
@@ -1,10 +1,15 @@
+import moment from 'moment';
 class Order {
-    constructor(id,items,totalAmount,date){
-       this.id=id,
-       this.items=items,
-       this.totalAmount=totalAmount,
-       this.date=date 
-    }
+  constructor(id, items, totalAmount, date) {
+    (this.id = id),
+      (this.items = items),
+      (this.totalAmount = totalAmount),
+      (this.date = date);
+  }
+
+  get readableDate() {
+    return moment(this.date).format('MMMM DD YYYY, hh:mm');
+  }
 }
 
-export default Order
+export default Order;
--- screens/shop/OrderScreens.js
+++ screens/shop/OrderScreens.js
@@ -1,35 +1,42 @@
 import React from "react";
-import { View, FlatList, StyleSheet, Text,Platform } from "react-native";
+import { View, FlatList, StyleSheet, Text, Platform } from "react-native";
 import { useSelector } from "react-redux";
-import { HeaderButtons,Item } from "react-navigation-header-buttons";
-import HeaderButton from '../../components/UI/HeaderButton'
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import HeaderButton from "../../components/UI/HeaderButton";
+import OrderItem from "../../components/shop/OrderItems";
 const OrderScreen = (props) => {
   const Orders = useSelector((state) => state.orders.orders);
   return (
-     <FlatList
+    <FlatList
       data={Orders}
-      keyExtractor={item => item.id}
-      renderItem={itemData => (
-      <Text>{itemData.item.totalAmount}</Text>
+      keyExtractor={(item) => item.id}
+      renderItem={(itemData) => (
+        <OrderItem
+          amount={itemData.item.totalAmount}
+          date={itemData.item.readableDate}
+        />
       )}
-     />
+    />
   );
 };
 
 export const ScreenOptions = (NavData) => {
-     return{
-         headerTitle:'Your Order',
-         headerLeft : () =>{
-             return(
-                <HeaderButtons HeaderButtonComponent={HeaderButton}>
-                <Item title='Cart' iconName={Platform.OS === 'android' ? 'md-menu' : 'ios-menu'}  
-                onPress={()=>{
-                  NavData.navigation.openDrawer()
-                }} />
-            </HeaderButtons>
-             )
-         }
-     }
+  return {
+    headerTitle: "Your Order",
+    headerLeft: () => {
+      return (
+        <HeaderButtons HeaderButtonComponent={HeaderButton}>
+          <Item
+            title="Cart"
+            iconName={Platform.OS === "android" ? "md-menu" : "ios-menu"}
+            onPress={() => {
+              NavData.navigation.openDrawer();
+            }}
+          />
+        </HeaderButtons>
+      );
+    },
+  };
 };
 
 const styles = StyleSheet.create({});
